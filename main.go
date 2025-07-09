cde725d79ba6708b4123ac2e981dffafbc44272e
Go-roro <royceletters@gmail.com>
2025-07-09 12:33:10 +0900

    feature: Create task model

--- main.go
+++ main.go
@@ -2,12 +2,6 @@ package main
 
 import "fmt"
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
-
 func main() {
 	fmt.Println("Hello, World!")
 }
-
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
